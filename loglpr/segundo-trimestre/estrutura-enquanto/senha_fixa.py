ef7e408f3465e9fbcd2482fcf91126ae94b29f91
João Guilherme <aluno6222@gmail.com>
2026-09-14 09:25:25 -0300

    Delete loglpr/segundo-trimestre/estrutura-enquanto directory

--- loglpr/segundo-trimestre/estrutura-enquanto/senha_fixa.py
+++
@@ -1,9 +0,0 @@
-senha = 2002
-condicao = True
-while condicao:
-    senha_digitada = int(input("Digite a senha: "))
-    if senha_digitada == senha:
-        print("Acesso permitido!")
-        condicao = False
-    else:
-        print("Senha invalida! Tente novamente.")
